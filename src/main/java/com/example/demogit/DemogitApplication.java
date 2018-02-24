a7fb78ea1833da593bba6f443f3acafb438dae88
Sreevidhya <sreevidhya.sethuraman@walgreens.com>
2018-02-24 00:32:51 -0600

    Initial commit

---
+++ src/main/java/com/example/demogit/DemogitApplication.java
@@ -0,0 +1,14 @@
+package com.example.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitApplication {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Git Demo!!!!");
+		SpringApplication.run(DemogitApplication.class, args);
+	}
+}
